0c4085d078802e37639c343449e2264ca6ece19b
virginie <virginiegodfrin@gmail.com>
2020-10-19 19:42:25 +0200

    structure des vues

--- resources/views/bonjour.blade.php
+++ resources/views/bonjour.blade.php
@@ -1,1 +1,17 @@
-<h1>Bonjour {{$prenom}} </h1>
+@extends('header')
+
+<div class='container'>
+    @section('titre')
+        <div class='jumbotron'>
+           <h1>Bonjour {{$prenom}} </h1> 
+        </div>
+    @endsection
+    <div class="row">
+        @section('contenu')
+            <p class="col-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, aliquam, autem, obcaecati, distinctio atque quae soluta ut vel itaque cum non amet aliquid et excepturi doloremque quas nihil sequi accusantium?</p>
+            <p class="col-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, aliquam, autem, obcaecati, distinctio atque quae soluta ut vel itaque cum non amet aliquid et excepturi doloremque quas nihil sequi accusantium?</p>
+            <p class="col-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, aliquam, autem, obcaecati, distinctio atque quae soluta ut vel itaque cum non amet aliquid et excepturi doloremque quas nihil sequi accusantium?</p>
+        @endsection
+    </div>
+</div>
+@extends('footer')   
